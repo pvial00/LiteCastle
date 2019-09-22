bdcf24dbeab8f2ad58eee8c52c76c3baf038be84
Karl Zander <pvial@Karls-MacBook-Air.local>
2019-09-21 23:49:28 -0700

    LiteCastle smoke test

--- src/scripts/castlesmoke.py
+++ src/scripts/castlesmoke.py
@@ -1,7 +1,7 @@
 import subprocess, os, time, sys
 
 # Smoke test all algorithms
-#This script assumes that you have the castle, darkpass, ganjasum
+#This script assumes that you have the lcastle, darkpass, ganjasum
 
 inputfile = "testfile1"
 testfile_size = 1048576
@@ -23,7 +23,7 @@ def geninputfile():
     f.close()
 
 def getalgorithms():
-    cmd = ['castle']
+    cmd = ['lcastle']
     out = subprocess.check_output(cmd)
     linestmp = out.split("\n")
     lines = linestmp[5:]
@@ -34,36 +34,31 @@ def getalgorithms():
             algorithms.append(algorithm)
     return algorithms
 
-geninputfile()
-algorithms = getalgorithms()
-algorithm_count = len(algorithms)
 c = 0
 
-for algorithm in algorithms:
+for x in range(1):
     key = getkey(passlen)
     print key
-    cmdenc = ['castle', algorithm, '-e', inputfile, '.b1' , key]
+    cmdenc = ['lcastle', '-e', inputfile, '.b1' , key]
     try:
         out = subprocess.check_output(cmdenc)
     except subprocess.CalledProcessError as ier:
-        print algorithm + " failed to encrypt"
+        print "Failed to encrypt"
     else: 
-        print algorithm + " encrypting" + out
+        print "Encrypting" + out
         cmdhash = ['ganjasum', inputfile]
         h1 = subprocess.check_output(cmdhash)
-        cmddec = ['castle', algorithm, '-d', '.b1','.b2' , key]
+        cmddec = ['lcastle','-d', '.b1','.b2' , key]
         out = subprocess.check_output(cmddec)
-        print algorithm + " decrypting" + out
+        print "Decrypting" + out
         cmdhash = ['ganjasum', '.b2']
         h2 = subprocess.check_output(cmdhash)
         if h1 != h2:
             print inputfile + " failed to decrypt!"
+            exit(1)
         else:
            c += 1
-if c == algorithm_count:
-    print "Success all algorithms check out"
-else:
-    print "One or more algorithms failed"
+print "Success all algorithms check out"
 cmdrm = ['rm', inputfile]
 out = subprocess.check_output(cmdrm)
 cmdrm = ['rm', '.b1']
